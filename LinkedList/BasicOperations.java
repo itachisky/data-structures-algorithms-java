bc96d9461b1bd58fed709707f8d415db1df9ae87
itachisky <akashsky2107@gmail.com>
2024-02-14 06:53:39 +0530

    problem: group anagrams

---
+++ LinkedList/BasicOperations.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+public class BasicOperations {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node (int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+
